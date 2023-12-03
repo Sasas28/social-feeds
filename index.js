d4957390fc75f3ae1c63c0e3aaeb87fd11ce3633
Sasas28 <ajasterzamora28@gmail.com>
2023-12-04 05:08:55 +0800

    added images folder and process upload image

--- index.js
+++ index.js
@@ -8,16 +8,37 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
+app.use("/images", express.static(path.join(__dirname, "public/images")))
 
 //middleware
 app.use(cors())
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+})
+  
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploded successfully");
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
