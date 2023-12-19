cbffd5d319d2a3279bb6aa0fabe4a14d6d650844
Sasas28 <ajasterzamora28@gmail.com>
2023-12-19 19:17:25 +0800

    modify way of connecting to database

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path = require("path");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL).then((_res) => console.log("Connected to DB"));
 app.use("/images", express.static(path.join(__dirname, "public/images")))
 
 //middleware
